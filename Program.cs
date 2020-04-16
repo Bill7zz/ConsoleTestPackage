f0c35b73f0e6d61c2b4c0258e2ce22c6dce748a5
Bill7zz <billytagzz@gmail.com>
2020-04-16 11:15:02 -0300

    Init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTestPackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# GitLearn
-For practices Git command
